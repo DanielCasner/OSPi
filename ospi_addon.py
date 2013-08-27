fc15147ce259e3836edc2574e1898acfb5c66a94
Daniel Casner <daniel@danielcasner.org>
2013-08-27 17:06:00 +0000

    DHT temperature sensor and uptime addons.

--- ospi_addon.py
+++ ospi_addon.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python
 import ospi
+import subprocess
 
   #### Add any new page urls here ####
-ospi.urls.extend(['/c1', 'ospi_addon.custom_page_1']) # example: (['/c1', 'ospi_addon.custom_page_1', '/c2', 'ospi_addon.custom_page_2', '/c3', 'ospi_addon.custom_page_3'])
+ospi.urls.extend(['/dht', 'ospi_addon.dht', '/uptime', 'ospi_addon.uptime'])
+
+##print ospi.urls # for testing
 
   #### add new functions and classes here ####
-  ### Example custom class ###
-class custom_page_1:
+
+  ### Example custom class. Uncomment and use to create a new web page. ###
+##class custom_page_1:
+##   """Add description here"""
+##   def GET(self):
+##      custpg = '<!DOCTYPE html>\n'
+##      #Insert Custom Code here.
+##      return custpg
+
+
+class dht:
    """Add description here"""
    def GET(self):
-      custpg = '<!DOCTYPE html>\n'
-      #Insert Custom Code here.
-      custpg += '<body>Hello form an ospi_addon program!</body>'
+      custpg = '<!DOCTYPE html>\n<html>\n<body>\n<pre>\n'
+      p = subprocess.Popen(['Adafruit_DHT', '11', '24'], stdout=subprocess.PIPE)
+      p.wait()
+      custpg += p.communicate()[0]
+      custpg += '\n</pre>\n</body>\n</html>\n'
+      return custpg
+
+class uptime:
+   """Add description here"""
+   def GET(self):
+      custpg = '<!DOCTYPE html>\n<html>\n<body>\n<pre>\n'
+      p = subprocess.Popen(['uptime'], stdout=subprocess.PIPE)
+      p.wait()
+      custpg += p.communicate()[0]
+      custpg += '\n</pre>\n</body>\n</html>\n'
       return custpg
 
 
